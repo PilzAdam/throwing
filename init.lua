c74812bee3751b130c6130adb36912a8e167634c
PilzAdam <adam-k@outlook.com>
2012-10-13 18:28:42 +0200

    The arrow at the right of the bow is taken instead of the first arrow in the inventory

--- init.lua
+++ init.lua
@@ -7,23 +7,21 @@ arrows = {
 }
 
 local throwing_shoot_arrow = function(itemstack, player)
-	for _,stack in ipairs(player:get_inventory():get_list("main")) do
-		for _,arrow in ipairs(arrows) do
-			if stack:get_name() == arrow[1] then
-				player:get_inventory():remove_item("main", arrow[1])
-				local playerpos = player:getpos()
-				local obj = minetest.env:add_entity({x=playerpos.x,y=playerpos.y+1.5,z=playerpos.z}, arrow[2])
-				local dir = player:get_look_dir()
-				obj:setvelocity({x=dir.x*19, y=dir.y*19, z=dir.z*19})
-				obj:setacceleration({x=dir.x*-3, y=-10, z=dir.z*-3})
-				obj:setyaw(player:get_look_yaw()+math.pi)
-				minetest.sound_play("throwing_sound", {pos=playerpos})
-				if obj:get_luaentity().player == "" then
-					obj:get_luaentity().player = player
-				end
-				obj:get_luaentity().node = player:get_inventory():get_stack("main", 1):get_name()
-				return true
+	for _,arrow in ipairs(arrows) do
+		if player:get_inventory():get_stack("main", player:get_wield_index()+1):get_name() == arrow[1] then
+			player:get_inventory():remove_item("main", arrow[1])
+			local playerpos = player:getpos()
+			local obj = minetest.env:add_entity({x=playerpos.x,y=playerpos.y+1.5,z=playerpos.z}, arrow[2])
+			local dir = player:get_look_dir()
+			obj:setvelocity({x=dir.x*19, y=dir.y*19, z=dir.z*19})
+			obj:setacceleration({x=dir.x*-3, y=-10, z=dir.z*-3})
+			obj:setyaw(player:get_look_yaw()+math.pi)
+			minetest.sound_play("throwing_sound", {pos=playerpos})
+			if obj:get_luaentity().player == "" then
+				obj:get_luaentity().player = player
 			end
+			obj:get_luaentity().node = player:get_inventory():get_stack("main", 1):get_name()
+			return true
 		end
 	end
 	return false
